85c6e773d445ffd2e1c86e63bdd06dae11b7c440
Nguyễn Tuấn Anh <baoawm2@gmail.com>
2026-01-09 08:00:24 +0700

    tính năng nghe thử audio

--- public/admin/js/script.js
+++ public/admin/js/script.js
@@ -6,12 +6,51 @@ if (uploadImage) {
     "[upload-image-preview]"
   );
 
+  let currentImageUrl = null;
+
   uploadImageInput.addEventListener("change", (e) => {
     if (e.target.files.length) {
+      if (currentImageUrl) URL.revokeObjectURL(currentImageUrl);
       const image = URL.createObjectURL(e.target.files[0]);
 
+      currentImageUrl = image;
+
       uploadImagePreview.src = image;
+      uploadImagePreview.style.display = "inline-block";
+    } else {
+      if (currentImageUrl) URL.revokeObjectURL(currentImageUrl);
+      currentImageUrl = null;
+      uploadImagePreview.src = "";
+      uploadImagePreview.style.display = "none";
     }
   });
 }
 // End Upload Image
+
+// Upload Audio
+
+const uploadAudio = document.querySelector("[upload-audio]");
+if (uploadAudio) {
+  const uploadAudioInput = uploadAudio.querySelector("[upload-audio-input]");
+  const uploadAudioPlay = uploadAudio.querySelector("[upload-audio-play]");
+
+  let currentAudioUrl = null;
+
+  uploadAudioInput.addEventListener("change", (e) => {
+    if (e.target.files.length) {
+      if (currentAudioUrl) URL.revokeObjectURL(currentAudioUrl);
+      const audio = URL.createObjectURL(e.target.files[0]);
+
+      currentAudioUrl = audio;
+
+      uploadAudioPlay.src = audio;
+      uploadAudioPlay.style.display = "block";
+      uploadAudioPlay.load();
+    } else {
+      if (currentAudioUrl) URL.revokeObjectURL(currentAudioUrl);
+      currentAudioUrl = null;
+      uploadAudioPlay.src = "";
+      uploadAudioPlay.style.display = "none";
+    }
+  });
+}
